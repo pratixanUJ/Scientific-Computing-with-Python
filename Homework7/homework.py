66b426d3e765069f91ada0bc4de257f602cfc6d1
pratixanUJ <101064553+pratixanUJ@users.noreply.github.com>
2022-07-02 22:21:55 +0200

    Update homework.py

--- Homework7/homework.py
+++ Homework7/homework.py
@@ -1,3 +1,8 @@
+# EXERCISE 7.1
+# -----------------------------------------------
+print('Exercise 7.1')
+print('------------')
+
 from vector3D import Vector
 
 def find_axis(v1, v2):
@@ -14,3 +19,43 @@ V1 = Vector(1,2,-3)
 V2 = Vector(3,1,-5)
 find_axis(V1,V2)
 
+# EXERCISE 7.2
+# -----------------------------------------------
+print('Exercise 7.2')
+print('------------')
+
+import random
+
+def inf_a():
+    i = 0
+    while True:
+        yield i
+        i += 1
+        if i == 1:
+            yield i
+            i-=1
+            
+print(inf_a())
+
+def inf_b():
+    list = [0,1]
+    while True:
+        yield random.choice(list)
+
+print(inf_b())
+
+def inf_c():
+    i = 0
+    while True:
+        for j in range(4):
+            if j == 0:
+                yield i
+            elif j == 1:
+                yield i+1
+            elif j == 2:
+                yield i
+            elif j == 3:
+                yield i-1
+
+print(inf_c())
+                
