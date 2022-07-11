f0a0c9c762afa52e82e20edf519ff0d4b305fcfb
pratixanUJ <101064553+pratixanUJ@users.noreply.github.com>
2022-07-11 16:07:33 +0200

    scatter plot modified

--- Homework9/scatter.py
+++ Homework9/scatter.py
@@ -7,27 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-Xg = []
-Yg = []
-Xr = []
-Yr = []
-areag = []
-arear = []
-for i in range(100):
-    x = random.random()
-    y = random.random()
-    if x**2+y**2<1:
-        Xg.append(x)
-        Yg.append(y)
-        areag.append(100*(x+y))
-    else:
-        Xr.append(x)
-        Yr.append(y)
-        arear.append(100*(x+y))
-    
-    
-plt.scatter(Xg,Yg,c='g',s=areag)
-plt.scatter(Xr,Yr,c='r',s=arear)
-plt.savefig('Scatter.jpg')
+N = 100
+x = np.random.rand(N)
+y = np.random.rand(N)
+plt.scatter(x,y,c=np.where(x**2+y**2<1,'g','r'),s=(x+y)*100)
 
+plt.savefig('Scatter.jpg')
 print('Scatter plot saved to Scatter.jpg!')
