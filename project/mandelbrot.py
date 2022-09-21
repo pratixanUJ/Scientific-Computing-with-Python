d692a35de5788a07639bb8ecd75b98359843be18
pratixanUJ <101064553+pratixanUJ@users.noreply.github.com>
2022-09-21 14:24:36 +0200

    Update mandelbrot.py

--- project/mandelbrot.py
+++ project/mandelbrot.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import agrparse
+
+#To avoid warnings thrown due to vectorization
+np.warnings.filterwarnings("ignore")
 
 # Defining the recursion relation for the Mandelbrot set: z(i)=z(i-1)**2 + C
 def z(i,C):
@@ -15,19 +20,24 @@ def complex_grid(xmin, xmax, ymin, ymax, grid_points):
     return Re[np.newaxis,:]+Im[:,np.newaxis]*1j
 
 # Checking convergence of the complex point C
-def converge(c,iter):
+def converge(c,num_iter):
     z = 0 #initial point
     j = 0
-    while j<=iter:
+    while j<=num_iter:
         z = z**2 + c
         j+= 1
     return abs(z) <= 2
 
+#Passing the parameter values as arguments
+parser = argparse.ArgumentParser(description='Argument parameters to generate the Mandelbrot Set')
+parser.add_argument('-N',type=int,help='No. of iterations to run')
+parser.add_argument('-d',type=int,help='No. of grid points')
+args = parser.parse_args()
 
-c = complex_grid(-2,1,-2,2,grid_points=1024) #creating the grid of complex numbers to be used for generating the set
+c = complex_grid(-2,1,-2,2,args.d) #creating the grid of complex numbers to be used for generating the set
 
 # Plotting
-plt.imshow(converge(c,iter=50), cmap = "RdBu")
+plt.imshow(converge(c,args.N), cmap = "RdBu")
 plt.axis("off")
 plt.title("Mandelbrot Set")
 plt.savefig("Mandelbrot.jpg")
